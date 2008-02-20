a5ced0e4b6067530ec52a3698a61063147939640
ceteri <ceteri@bd393058-8d46-0410-9db1-ab504f931115>
2008-02-20 02:04:56 +0000

    initial src checkin

---
+++ trunk/x.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## change these two lines
+VERSION=0.15.3
+HADOOP=~/hadoop-${VERSION}
+
+## build the JAR file
+
+javac -classpath ${HADOOP}/hadoop-${VERSION}-core.jar -d classes JyteRank.java 
+jar -cvf jyterank.jar -C classes/ .
+
+## setup the HDFS directory structure
+
+rm -rf input from2to prevrank elemrank thisrank
+${HADOOP}/bin/hadoop fs -put cred.txt input/cred01
+
+## run the jobs
+
+${HADOOP}/bin/hadoop jar jyterank.jar JyteRank input from2to prevrank elemrank thisrank
+
+${HADOOP}/bin/hadoop fs -ls prevrank
+#${HADOOP}/bin/hadoop fs -cat prevrank/part-00000
