7779a76a04fd8655b823893da0d7f633fc5ed262
ceteri <ceteri@bd393058-8d46-0410-9db1-ab504f931115>
2008-03-07 01:49:12 +0000

    adding Ant build script

--- trunk/jyte/x.sh
+++ trunk/jyte/x.sh
@@ -1,27 +1,10 @@
 #!/bin/bash
 
-# run Hadoop iterations to approximate jyte cred points
-#
-# Paco NATHAN http://code.google.com/p/ceteri-mapred/
-
 ## change the following two lines as needed
 VERSION=0.15.3
 HADOOP=~/hadoop-${VERSION}
 
-## build the JAR file
+## compare N results
 
-javac -classpath ${HADOOP}/hadoop-${VERSION}-core.jar -d classes src/org/ceteri/JyteRank.java 
-jar -cvf jyterank.jar -C classes/ .
-
-## setup the HDFS directory structure
-
-rm -rf input from2to prevrank elemrank thisrank
-${HADOOP}/bin/hadoop fs -put cred.txt input/cred01
-
-## run the jobs
-
-${HADOOP}/bin/hadoop jar jyterank.jar org.ceteri.JyteRank input from2to prevrank elemrank thisrank
-
-## uncomment the following two line to compare N results
-#N=10
-#${HADOOP}/bin/hadoop fs -cat prevrank/part-00000 | head -${N} | ./crawl.pl > compare.tsv
+N=10
+${HADOOP}/bin/hadoop fs -cat prevrank/part-00000 | head -${N} | ./crawl.pl > compare.tsv
